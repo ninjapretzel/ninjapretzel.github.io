5db9da56cefe47e7b8c66f793f645402cd6a34e2
ninjapretzel <ninjapretzel@gmail.com>
2021-11-10 15:42:14 -0700

    Adds test output, auto-expanding failed test cases..

--- coding/common.js
+++ coding/common.js
@@ -13,8 +13,13 @@ function wait(ms) {
 async function pause(ms) { await wait(ms); }
 
 async function fetchJson(path) { 
-	let req = await fetch(path);
-	return await req.json();	
+	
+	const headers = new Headers();
+	headers.append('pragma', 'no-cache');
+	headers.append('cache-control', 'no-cache');
+	const req = new Request(path);
+	const res = await fetch(req, headers);
+	return await res.json();
 }
 function SimConsole() {
 	this.buffer = "";
@@ -24,9 +29,11 @@ function SimConsole() {
 }
 
 let lesson = {
-	Category: "Untitled Category",
-	Lesson: "Untitled Lesson",
-	LessonText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	Content: {
+		Category: "Untitled Category",
+		Lesson: "Untitled Lesson",
+		LessonText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	},
 	
 	Preamble: "function placeholder(a,b,c) {",
 	InitialCode: "\n\t// Edit Me!\n",
@@ -34,7 +41,9 @@ let lesson = {
 	TestCases: [
 		{ 
 			args: ["args", "value", "or", "array"], 
+			expectReturnValue: true,
 			expected: "expectedReturn", 
+			expectConsoleOutput: true,
 			expectedConsole: "expected console output" 
 		},
 	],
@@ -57,17 +66,114 @@ function measure(str) {
 	}
 	return {line,ch};
 }
+function upgradeBrowser() {
+	$(".main").addClass("hidden");
+	$(".upgrade").removeClass("hidden");
+}
+
+function failedToLoad() {
+	$(".main").addClass("hidden");
+	$(".failedToLoad").removeClass("hidden");
+}
+function showLesson() {
+	for (let x in lesson.Content) {
+		const element = $("#"+x);
+		if (element.length != 0) { 
+			element.html(lesson.Content[x]);
+		}	
+	}
+	rerenderTestCases();
+}
+
+function rerenderTestCases() {
+	$("#output").empty();
+	renderTestCases();
+}
+
+
+// May allow for custom rendering of test cases, 
+// if lessons want to provide custom solutions for this.
+let renderTestCases = function() {
+	function passFail(result, key) {
+		if (result) {
+			if (result[key]) { return 1; }
+			return 2;
+		}
+		return 0;
+	}
+	const passFailColors = [ "blue-grey", "green", "red" ];
+	
+	const output = $("#output");
+	let i = 0;
+	console.log("rendering tests in",lesson.TestCases);
+	for (let test of lesson.TestCases) {
+		const result = results[i++];
+		const element = $("<li>");
+		const header = $("<div>");
+		header.addClass("collapsible-header card blue-grey test");
+		header.append("Test Case #"+i+"<br/><pre>args="+JSON.stringify(test.args)+"</pre>");
+		
+		const body = $("<div>");
+		body.addClass("collapsible-body card row blue-grey test");
+		element.append(header);
+		element.append(body);
+		let wasFailure = false;
+		
+		if (test.expectReturnValue) {
+			let text = "Expecting result of " + JSON.stringify(test.expected);	
+			if (result) {
+				text += "\nGot " + JSON.stringify(result.returnVal);
+				if (!result.matchedReturnValue) { wasFailure = true; }
+			} else {
+				text += "\nNot run yet...";	
+			}
+			const div = $("<div>");
+			div.addClass("col s12 card test");
+			div.addClass(passFailColors[passFail(result, "matchedReturnValue")]);
+			div.append($("<pre>"+text+"</pre>"));
+			body.append(div);
+		}
+		
+		if (test.expectConsoleOutput) {
+			const div = $("<div>");
+			div.addClass("col s12 card test");
+			div.addClass(passFailColors[passFail(result, "matchedConsoleOutput")]);
+			
+			
+			div.append("<span>Expected Console Output:</span>");
+			div.append("<pre>"+test.expectedConsole+"</pre>");
+			if (result) {
+				if (!result.matchedConsoleOutput) { wasFailure = true; }
+				if (result.consoleOutput.length == 0) { 
+					div.append("<span>Got: </span><pre>(nothing)</pre>");	
+					
+				} else {
+					div.append("<span>Got:</span><pre>"+result.consoleOutput+"</pre>");	
+				}
+			} else {
+				div.append("<span>Not run yet</span><pre>...</pre>");
+			}
+			body.append(div);
+		}
+		output.append(element);
+		
+		
+		if (wasFailure) {
+			console.log(i, "was failure");
+			$('.collapsible').collapsible('open', i-1);
+		}
+			
+		
+	}
+}
 
 $(document).ready(async ()=>{
-	if (!localStorage) {
-		//alert("You need to use a modern browser to use this website. Please upgrade to Firefox, Opera, or *shudder* Chrome.");	
-		$(".main").addClass("hidden");
-		$(".upgrade").removeClass("hidden");
-		return;
-	}
+	if (!localStorage) { upgradeBrowser(); return; }
 	let jsToLoad = "";
 	let res = await fetchJson("data/test.json")
-	if (res) { lesson = res; }
+	
+	if (res) { lesson = res; showLesson(); }
+	else { failedToLoad(); return; }
 	
 	const startMarker = { line: -1, ch:-1 }
 	if (lesson.Preamble) { jsToLoad += lesson.Preamble; }
@@ -111,6 +217,7 @@ $(document).ready(async ()=>{
 		}, 100);
 			
 		try { $('.tooltipped').tooltip(); } catch (e) { console.warn(e); }
+		try { $('.collapsible').collapsible( {accordion: false} ); } catch (e) { console.warn(e); }
 	}, 100);
 	
 });
@@ -124,38 +231,27 @@ async function execScript() {
 	runId = interp.runId;
 	let script = codeEditor.getValue();
 	let result = null;
-	// try {
-	// 	runTask = evaluate(script, "dynamic", 1, injectedFunctions);
-	// 	result = await runTask;
-	// 	M.toast({html: "Run Finished.", classes:"green", displayLength: 2000  } );
-		
-	// } catch (e) {
-	// 	if (e === INTERRUPTED) {
-	// 		M.toast({html:`${e}.`, classes:"yellow black-text", displayLength: 1000});
-	// 	} else {
-	// 		M.toast({html:`Script Error. ${e}`, classes:"red" })
-	// 		console.error("e");
-	// 	}
-	// }
 	
 	const testCode = lesson.TestCode;
 	for (let i = 0; i < lesson.TestCases.length; i++) {
 		const test = lesson.TestCases[i];
 		
 		const args = test.args;
+		const expectExact = test.expectExact;
 		const expected = test.expected;
 		const expectedConsole = test.expectedConsole;
 		
 		const code = script 
 			+ "\nconst args = " + JSON.stringify(args)
 			+ "\n" + testCode;
-		console.log(`executing:\n--------------\n${code}\n---------------\n`);
-		
+			
+		// console.log(`executing:\n--------------\n${code}\n---------------\n`);
+		let returnVal = undefined;
 		simConsole.clear();
 		const start = new Date().getTime();
 		try {
 			runTask = evaluate(code, "dynamic", 1, injectedFunctions);
-			result = await runTask;
+			returnVal = result = await runTask;
 			//M.toast({html: "Run Finished.", classes:"green", displayLength: 2000  } );
 		} catch (e) {
 			if (e === INTERRUPTED) {
@@ -166,16 +262,20 @@ async function execScript() {
 			}
 		}
 		const end = new Date().getTime();
+		const consoleOutput = simConsole.buffer;
 		const elapsedMS = end-start;
-		const matchedReturnValue = !expected || result === expected;
+		const matchedReturnValue = expectExact 
+			? result === expected
+			: (!expected || result === expected);
+			
 		const matchedConsoleOutput = !expectedConsole || simConsole.buffer === expectedConsole;
 		
-		const res = { elapsedMS, matchedReturnValue, matchedConsoleOutput }
-		console.log(res);
+		const res = { elapsedMS, returnVal, consoleOutput, matchedReturnValue, matchedConsoleOutput }
+		// console.log(res);
 		results[i] = res;
 	}
 	
-	console.log(results);
+	rerenderTestCases();
 }
 
 async function resetScriptExec() {
@@ -194,10 +294,6 @@ async function resetScriptExec() {
 	}
 	
 	// TODO: Any other reset tasks...
-}
-
-async function resetTestCases() {
-		
 }
 
 function urlParam(parameterName) {
